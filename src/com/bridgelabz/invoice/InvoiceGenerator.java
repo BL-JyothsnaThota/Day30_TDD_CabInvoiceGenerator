1075bee246b8dcd3ccafdfcd3ad2143b98623e0a
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-06-06 11:53:13 +0530

    returning total fare of the journey

---
+++ src/com/bridgelabz/invoice/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.invoice;
+
+public class InvoiceGenerator {
+
+    private static final int COST_PER_KM = 10;
+    private static final int COST_PER_MIN = 1;
+    private static final int MIN_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double fare = distance * COST_PER_KM + time * COST_PER_MIN;
+
+        return Math.max(fare, MIN_FARE);
+    }
+}
